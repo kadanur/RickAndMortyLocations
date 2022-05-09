6bb064211b861cb7b1f6a67d1841021aa958a776
kadanur <mkadanur@gmail.com>
2022-05-09 22:22:56 +0300

    Initial Commit

---
+++ RickAndMortyLocations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyLocations
+//
+//  Created by M Kaan Adanur on 9.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
